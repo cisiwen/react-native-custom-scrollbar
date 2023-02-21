2172f468676110ac2d8604e021a76293ea1f0a70
wweng <wweng@wwengs-MacBook-Pro.local>
2023-02-21 20:09:15 +1100

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,18 +1,67 @@
-import { StyleSheet, Text, View } from 'react-native';
-
+import { FlatList, ListRenderItemInfo, NativeScrollEvent, NativeSyntheticEvent, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { useAnimatedScrollHandler, useSharedValue } from 'react-native-reanimated';
+import ImmersiveMode from 'react-native-immersive-mode';
+import FullScreenChz from 'react-native-fullscreen-chz';
+import data from "./dataProvider";
+const renderItem=(item:ListRenderItemInfo<string>)=>{
+  return <View style={styles.listItem}>
+    <Text style={styles.listItemText}>{item.item}</Text>
+  </View>
+}
 export default function App() {
+ 
+  const lastContentOffset = useSharedValue(0);
+  const isUp = useSharedValue(false);
+  
+ 
+  const onScroll=(event:NativeSyntheticEvent<NativeScrollEvent>)=>{
+    if (lastContentOffset.value > event.nativeEvent.contentOffset.y) {
+        console.log("down");
+        FullScreenChz.disable();
+    }
+    else if(lastContentOffset.value<event.nativeEvent.contentOffset.y) {
+      console.log("up");
+      FullScreenChz.enable();
+    }
+    lastContentOffset.value= event.nativeEvent.contentOffset.y;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+      onScroll={onScroll}
+      numColumns={1}
+      style={styles.list}
+      data={data}
+      renderItem={renderItem}
+      keyExtractor={(item)=>`key${item}`}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  listItemText:{
+    color:"#999999"
+  },
+  listItem:{
+    flex:1,
+    backgroundColor:"#282828",
+    padding:10,
+    paddingBottom:20,
+    paddingTop:20,
+    color:"#fff",
+    borderBottomColor:"#FFF",
+    borderBottomWidth:1
+  },
+  list:{
+    width:"100%"
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+ 
+
+ 
---
+++ dataProvider.ts
@@ -0,0 +1,5 @@
+var data:string[]=[];
+for(let i=0;i<100000;i++){
+    data.push(i.toString());
+}
+export default data;
